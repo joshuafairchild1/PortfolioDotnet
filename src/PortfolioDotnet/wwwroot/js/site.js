b15d07a55a912eb141a8674587f149c9a25115a3
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-25 15:15:54 -0700

    add background particle animation

---
+++ src/PortfolioDotnet/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    Particles.init({
+        selector: '.background',
+        sizeVariations: 4,
+        connectParticles: true,
+        minDistance: 170,
+        maxParticles: 300,
+        color: '#E7878A'
+    });
+};
