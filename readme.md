6d3afddc07af334f53c345ebdbd079a76dc3bcfe
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-25 10:55:02 -0700

    init and scaffold project with three controllers + corresponding view files, link dependencies

---
+++ readme.md
@@ -0,0 +1,48 @@
+# Personal Portfolio
+
+#### Joshua Fairchild, August 25th, 2017
+
+## Description
+
+Description coming soon
+
+## Prerequisites
+
+You will need the following software properly installed on your computer.
+
+* [Visual Studio 2015](https://www.visualstudio.com/vs/older-downloads/)
+<!-- * [Microsoft SQL Server](https://www.microsoft.com/en-in/sql-server/sql-server-downloads) -->
+
+## Installation/Database Setup
+
+* Clone this repository.
+
+  `$ git clone https://github.com/joshuafairchild1/PortfolioDotnet`
+
+<!-- * Open MS SQL Server and establish a local connection. -->
+
+* In Visual Studio, open and build the project.
+
+<!-- * Run the following commands in PowerShell to generate the database: -->
+
+  <!-- `$ cd .\src\MrFixIt\` -->
+
+  <!-- `$ dotnet ef database update 20170818192949_FixWorkerModelTypo` -->
+
+* You can now view the application by pressing Ctrl + F5 in Visual Studio, you will be navigated to the application in your web browser.
+
+
+## Built With
+
+* C#
+* ASP.NET Core MVC
+<!-- * Microsoft SQL Server -->
+<!-- * Identity (C# framework) -->
+* CSS
+* Materialize (CSS library)
+
+## License
+
+This project is licensed under the MIT License
+
+Copyright (c) Joshua Fairchild, 2017
---
+++ src/PortfolioDotnet/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace PortfolioDotnet
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStaticFiles();
+
+            loggerFactory.AddConsole();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async (error) =>
+            {
+                await error.Response.WriteAsync("You should not see this message. An error has occured.");
+            });
+        }
+    }
+}
